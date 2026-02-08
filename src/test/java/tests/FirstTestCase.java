61e3fa282826714ccadc4dca7c10a69fb005e969
Ada <adriannakopec6@gmail.com>
2026-02-08 23:28:30 +0100

    test setup

--- src/test/java/tests/FirstTestCase.java
+++ src/test/java/tests/FirstTestCase.java
@@ -1,10 +1,26 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v142.overlay.model.LineStyle;
+
+import java.util.List;
+
 public class FirstTestCase {
 
     public static void main(String[] args){
 
-        System.out.println("yo yoyo");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://google.com");
+        driver.findElement(By.id("")).sendKeys();
+        driver.findElement(By.name("")).click();
+        List<WebElement> headerLinks = driver.findElements(By.className(""));
+        System.out.println(headerLinks.size());
+        driver.findElement(By.tagName(""));
+
+        driver.close();
     }
 
 
